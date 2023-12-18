44e620f16a9a9338928031be2925a2e459150757
SuprimKoirala <suprim.079bei043@tcioe.edu.np>
2023-12-18 12:45:36 +0545

    some changes

--- after/plugin/dashboard.lua
+++
@@ -1,6 +0,0 @@
- local config = function()
-    require('dashboard').setup {
-      -- config
-    }
-  end
-
--- lua/supremacy/remap.lua
+++ lua/supremacy/remap.lua
@@ -42,33 +42,33 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
-    vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-    --for easier navigation of the panes
-    vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
-    vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
-    vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-    --yank to the system clipboard
-    --Copy
-    vim.keymap.set('v', '<leader>y', '"+y<cr>')
-    vim.keymap.set('n', '<leader>y', '"+y<cr>')
-    vim.keymap.set('n', '<leader>Y', '"+yg_<cr>')
-
-    --instead of the above, I can use the greatest remap ever
-    vim.keymap.set('x', '<leader>p', [["_dP]])
-
-    --move blocks of code
-    vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-    vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-    --replace all instances of the word the cursor is on
-    vim.keymap.set('n', 'R', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-    --splitting the windows
-    vim.keymap.set('n', '<leader>wv', ':vsplit<cr> :wincmd l<CR>')
-    vim.keymap.set('n', '<leader>wh', ':split<cr> :wincmd j<CR>')
-    --resizing windows
-    vim.keymap.set('n', '<c-Left>', ':vertical resize+3<cr>')
-    vim.keymap.set('n', '<c-Right>', ':vertical resize-3<cr>')
-    vim.keymap.set('n', '<c-Up>', ':resize+3<cr>')
-    vim.keymap.set('n', '<c-Down>', ':resize-3<cr>')
 end)
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+--for easier navigation of the panes
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+--yank to the system clipboard
+--Copy
+vim.keymap.set('v', '<leader>y', '"+y<cr>')
+vim.keymap.set('n', '<leader>y', '"+y<cr>')
+vim.keymap.set('n', '<leader>Y', '"+yg_<cr>')
+
+--instead of the above, I can use the greatest remap ever
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+--move blocks of code
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+--replace all instances of the word the cursor is on
+vim.keymap.set('n', 'R', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+--splitting the windows
+vim.keymap.set('n', '<leader>wv', ':vsplit<cr> :wincmd l<CR>')
+vim.keymap.set('n', '<leader>wh', ':split<cr> :wincmd j<CR>')
+--resizing windows
+vim.keymap.set('n', '<c-Left>', ':vertical resize+3<cr>')
+vim.keymap.set('n', '<c-Right>', ':vertical resize-3<cr>')
+vim.keymap.set('n', '<c-Up>', ':resize+3<cr>')
+vim.keymap.set('n', '<c-Down>', ':resize-3<cr>')
